238267664f887ae3fa32f6e7b782d0851439dc11
mattThurstan <brian7android@gmail.com>
2018-03-06 09:53:45 +0000

    v0.200 - stable
    2 PIR sensors, top and bottom of stairs

--- Main/Board/stairsLight1_A/pir.ino
+++ Main/Board/stairsLight1_A/pir.ino
@@ -1,139 +1,109 @@
-
+/*----------------------------pir----------------------------*/
 void loopPir() 
 {
-
-  if (_onOff) {
-  
-    for (byte i = 0; i < _pirTotal; i++) {
-      if (_onState[i] == 0) {
-        if (DEBUG) { Serial.println("LED state 0 - fading on"); }
-        if (i == 0) {  
-          ledsFadeOn_Top2Bot(); //pin 2 top
-        } else if (i == 1) { 
-          ledsFadeOn_Bot2Top(); //pin 3 bottom
-        }
-      } else {
-        _leds( ledSegment[0].first, ledSegment[0].last).fill_gradient(_topColorHSV, _botColorHSV);
-        if (DEBUG) { Serial.println("LED state 1 - on"); }
-      }
-    }
-  
-  } else {
-    
-    if (_pirLastTriggered == 0) {
-      if (_offState[0] == 0) {
-        ledsFadeOff_Bot2Top();
-      } else {
-        _leds.fadeToBlackBy(4); //should already be black, just in case
-      }
-    } else if (_pirLastTriggered == 1) {
-      if (_offState[1] == 0) {
-        ledsFadeOff_Top2Bot();
-      } else {
-        _leds.fadeToBlackBy(4); //should already be black, just in case
-      }
-    }
-    //_leds.fadeToBlackBy(4);
-  } //END if _onOff
-
-  for (byte i = 0; i < _pirTotal; i++) {
+  if (_timerRunning) {
+    //lights on hold timer
     unsigned long pirHoldCurMillis = millis();    //get current time
-    if( ( (unsigned long) (pirHoldCurMillis - _pirHoldPrevMillis[i]) >= _pirHoldInterval ) && _onOff) {
+    if( (unsigned long)(pirHoldCurMillis - _pirHoldPrevMillis) >= _pirHoldInterval ) {
       //when the time has expired, do this..
-      //note all led fade loops should complete within this time.
-      _onState[i] = 0;
-      _onOff = false;
-      _pirHoldPrevMillis[i] = millis();              //store the current time
+      if (_state == 1 || _state == 2) {
+        _state = 3;
+        if (DEBUG) { Serial.println("State set to 3"); }
+      }
+      _timerRunning = false;                      //disable itself
       if (DEBUG) { 
-        Serial.print("PIR ");
-        Serial.print(i);
-        Serial.println(" hold interval expired!"); 
+        Serial.print(".");
+        Serial.println("PIR hold interval expired. State set to 3 (fade off), Timer disabled."); 
       }
     }
   }
-    
-} //END loopPir
-
-void ledsFadeOn_All() { }
-
-void ledsFadeOn_Top2Bot() 
-{
-  for (byte i = ledSegment[0].first; i <= ledSegment[0].last; i++) {
-    _offState[0] = 0;
-    _leds[i] = _topColorHSV;
-    FastLED.show();
-    FastLED.delay(_ledRiseSpeed);
-    if (i == ledSegment[0].last) { _onState[0] = 1; }
+  
+  if (_state == 0) {
+    if (DEBUG) { Serial.println("State = 0 (off)"); }
+    //off
+    _leds.fadeToBlackBy(4);
+  } else if (_state == 1) {
+    if (DEBUG) { Serial.println("State = 1 (fade on)"); }
+    //fade on
+    fadeOn();
+  } else if (_state == 2) {
+    if (DEBUG) { Serial.println("State = 2 (on)"); }
+    //on
+  _leds(ledSegment[0].first, ledSegment[0].last).fill_gradient(_topColorHSV, _botColorHSV);
+  } else if (_state == 3) {
+    if (DEBUG) { Serial.println("State = 3 (fade off)"); }
+    //fade off
+    fadeOff();
   }
 }
 
-void ledsFadeOn_Bot2Top() 
-{
-  for (byte i = ledSegment[0].last; i >= ledSegment[0].first; i--) {
-    _offState[1] = 0;
-    _leds[i] = _botColorHSV;
-    FastLED.show();
-    FastLED.delay(_ledRiseSpeed);
-    if (i == ledSegment[0].first) { _onState[1] = 1; }
-  }
-}
-
-void ledsFadeOff_All() 
-{
-  //for(int i = 0; i < _ledNum; i++) { _leds[i].nscale8(250); }
-  _leds.fadeToBlackBy(4);
-}
-
-void ledsFadeOff_Top2Bot() 
-{
-  for (byte i = ledSegment[0].first; i <= ledSegment[0].last; i++) {
-    _leds[i] = CRGB::Black;
-    FastLED.show();
-    FastLED.delay(_ledRiseSpeed);
-    if (i == ledSegment[0].last) { _offState[1] = 1; }
-  }
-}
-
-void ledsFadeOff_Bot2Top() 
-{
-  for (byte i = ledSegment[0].last; i >= ledSegment[0].first; i--) {
-    _leds[i] = CRGB::Black;
-    FastLED.show();
-    FastLED.delay(_ledRiseSpeed);
-    if (i == ledSegment[0].first) { _offState[0] = 1; }
-  }
-}
-
-
-//
-
-//void loop() {
-//  meteorRain(0xff,0xff,0xff,10, 64, true, 30);
-//}
-/*
-void meteorRain(byte red, byte green, byte blue, byte meteorSize, byte meteorTrailDecay, boolean meteorRandomDecay, int SpeedDelay) {  
-  setAll(0,0,0);
- 
-  for(int i = 0; i < NUM_LEDS+NUM_LEDS; i++) {
-   
-   
-    // fade brightness all LEDs one step
-    for(int j=0; j<NUM_LEDS; j++) {
-      if( (!meteorRandomDecay) || (random(10)>5) ) {
-        fadeToBlack(j, meteorTrailDecay );        
+void fadeOn() {
+  _leds.fadeToBlackBy(4);       //just in case
+  if (_fadeOnDirection == 0) {
+    //fade on top to bottom
+    for (byte i = ledSegment[0].first; i <= ledSegment[0].last; i++) {
+      fadeShowLEDs(ledSegment[0].first, i);
+      if (i == ledSegment[0].last) { 
+        _state = 2; 
+        if (DEBUG) { Serial.println("State set to 2"); }
+        return;
       }
     }
-   
-    // draw meteor
-    for(int j = 0; j < meteorSize; j++) {
-      if( ( i-j <NUM_LEDS) && (i-j>=0) ) {
-        setPixel(i-j, red, green, blue);
+  } else if (_fadeOnDirection == 1) {
+    //fade on bottom to top
+    for (byte i = ledSegment[0].last; i >= ledSegment[0].first; i--) {
+      fadeShowLEDs(i, ledSegment[0].last);
+      if (i == ledSegment[0].first) { 
+        _state = 2; 
+        if (DEBUG) { Serial.println("State set to 2"); }
+        return;
       }
     }
-   
-    showStrip();
-    delay(SpeedDelay);
   }
+  //see if this works here.. see below
+  //_state = 2;
+  //if (DEBUG) { Serial.println("State set to 2"); }
 }
-*/
+
+void fadeOff() {
+  //_leds.fadeToBlackBy(4);
+  if (_pirLastTriggered == 0) {
+    //fade off bottom to top 
+    for (byte i = ledSegment[0].last; i >= ledSegment[0].first; i--) {
+      //_leds.fadeToBlackBy(4);               //fade em all
+      _leds = CRGB::Black;
+      fadeShowLEDs(ledSegment[0].first, i); //then switch back on the ones we want
+      if (i == ledSegment[0].first) {
+        _leds = CRGB::Black;                  //turn off the last pixel before changing state
+        _state = 0; 
+        if (DEBUG) { Serial.println("State set to 0"); }
+        return;
+      }
+    }
+  } else if (_pirLastTriggered == 1) {
+    //fade off top to bottom
+    for (byte i = ledSegment[0].first; i <= ledSegment[0].last; i++) {
+      //_leds.fadeToBlackBy(4);
+      _leds = CRGB::Black;
+      fadeShowLEDs(i, ledSegment[0].last);
+      if (i == ledSegment[0].last) { 
+        _leds = CRGB::Black;                  //turn off the last pixel before changing state
+        _state = 0; 
+        if (DEBUG) { Serial.println("State set to 0"); }
+        return;
+      }
+    }
+  }
+  //see if this works.. 
+  //due to FastLED non-blocking delay, this might, or not, only reach this after the increment loop has ended.
+  //whichever, amend the other one above or this one to match
+//  _state = 0;
+//  if (DEBUG) { Serial.println("State set to 0"); }
+}
+
+void fadeShowLEDs(byte low, byte high) {
+  _leds(low, high).fill_gradient(_topColorHSV, _botColorHSV);
+  FastLED.show();
+  FastLED.delay(_ledRiseSpeed);
+}
 
--- Main/Board/stairsLight1_A/stairsLight1_A.ino
+++ Main/Board/stairsLight1_A/stairsLight1_A.ino
@@ -1,58 +1,64 @@
 /*
-    'stairsLight1_A' by Thurstan. Standalone PIR sensor(s) which switches on LED strip.
+    'stairsLight1_A' by Thurstan. Test for LEDs controlled by motion sensors.
     Copyright (C) 2018  MTS Standish (Thurstan|mattKsp)
-
+    
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
+    
     This program is distributed in the hope that it will be usefull,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
+    
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    https://github.com/mattKsp/
+    
+    https://github.com/mattThurstan/
 */
 
-/*
+/* 
  * Arduino Pro Mini (5v, ATmega328, 16mhz clock speed, 32KB Flash, 2KB SRAM, 1KB EEPROM)
- * http://wiki.keyestudio.com/index.php/Ks0052_keyestudio_PIR_Motion_Sensor 
-*/
-
-boolean DEBUG = true;
+ * 
+ * serial 0,1
+ * interrupts 2,3
+ * PWM 3,5,6,9,10,11
+ * SPI 10(SS),11(MOSI),13(SCK)
+ * I2C A4(SDA),A5(SCL)
+ * 
+ * http://wiki.keyestudio.com/index.php/Ks0052_keyestudio_PIR_Motion_Sensor
+ */
+  
+  
+boolean DEBUG = false;
+const unsigned long _pirHoldInterval = 30000; //150000;  //15000=15 sec. 30000=30 sec. 150000=2.5 mins.
+unsigned long _ledRiseSpeed = 35;                 //speed at which the LEDs turn on (runs backwards)
 
 /*----------------------------arduino pins----------------------------*/
-
-const byte _pirTotal = 2;
-const byte _pirPin[_pirTotal] = { 2, 3 };   //2 top, 3 bottom
-const byte _ledDOut0Pin = 4;
+//2=top, 3=bottom - due to the way the LED strip is wired (top to bot) so thats the way the array goes..
+const byte _pirPin[2] = { 2, 3 };             //2 PIR sensors on interrupt pins (triggered on HIGH)
+const byte _ledDOut0Pin = 4;                  //FastLED strip
 
 /*----------------------------libraries----------------------------*/
-#include <EEPROM.h>                         //a few saved settings
 #include <FastLED.h>                        //WS2812B LED strip control and effects
 
 /*----------------------------system----------------------------*/
 const String _progName = "stairsLight1_A";
-const String _progVers = "0.124";             //
-//const int _mainLoopDelay = 0;               //just in case  - using FastLED.delay instead..
-#define UPDATES_PER_SECOND 0           //120      //main loop FastLED show delay - 1000/120
+const String _progVers = "0.200";               //
+//const int _mainLoopDelay = 0;                   //just in case  - using FastLED.delay instead..
+#define UPDATES_PER_SECOND 0           //120    //main loop FastLED show delay - 1000/120
 
 /*----------------------------PIR----------------------------*/
-boolean _onOff = false;            //global. this should init false, then get activated by input - on/off true/false
-byte _onState[_pirTotal] = { 0, 0 };
-byte _offState[_pirTotal] = { 0, 0 };
-const unsigned long _pirReadInterval = 10;
-byte _pirPrevState[_pirTotal] = { 0, 0 };
-const unsigned long _pirDebounceInterval = 310; //debounce for the PIR as it stays on 1 for a while
-unsigned long _pirDebouncePrevMillis[_pirTotal] = { 0, 0 };
-boolean _pirDebounceTriggered[_pirTotal] = { false, false };                    //latch for PIR debounce
-const unsigned long _pirHoldInterval = 150000;            //30000=30 sec. 150000=2.5 mins.
-unsigned long _pirHoldPrevMillis[_pirTotal] = { 0, 0 };
-byte _pirLastTriggered = 255;
+volatile boolean _onOff = false;              //global. this should init false, then get activated by input - on/off true/false
+byte _state = 0;                              //0-Off, 1-Fade On, 2-On, 3-Fade Off
+//direction for fade on/off determined by last pir triggered
+//might not need to debounce PIR input if using RISING on interrupt
+//CHECK - think PIR stays HIGH for a duration ( ??? is default, 3m dist and 25ms hold ??? )
+volatile unsigned long _pirHoldPrevMillis = 0;
+volatile byte _pirLastTriggered = 255;        //last PIR sensor triggered (0=top or 1=bottom)
+volatile boolean _timerRunning = false;       //is the hold timer in use?
+volatile byte _fadeOnDirection = 255;         //direction for fade on loop. 0=fade down the stairs (top to bot), 1=fade up the stairs (bot to top).
 
 /*----------------------------LED----------------------------*/
 typedef struct {
@@ -64,18 +70,15 @@ const byte _segmentTotal = 1;                     //
 const byte _ledNum = 108;                         //
 LED_SEGMENT ledSegment[_segmentTotal] = {
   /*{ 0, 16, 17 },*/ 
+  /*{ 0, 9, 10 },*/
   { 0, 107, 108 },
 };
 CRGBArray<_ledNum> _leds;                         //CRGBArray means can do multiple '_leds(0, 2).fadeToBlackBy(40);' as well as single '_leds[0].fadeToBlackBy(40);'
 
 byte _ledGlobalBrightnessCur = 255;               //current global brightness
-unsigned long _ledRiseSpeed = 35;                 //speed at which the LEDs turn on (runs backwards)
 
 CHSV _topColorHSV( 50, 80, 159 );                 //0, 0, 200
 CHSV _botColorHSV( 50, 80, 159 );                   //0, 0, 200
-//CRGB _startColorRGB( 3, 144, 232 );
-//CRGB _endColorRGB( 249, 180, 1 );
-
 
 /*----------------------------MAIN----------------------------*/
 void setup()
@@ -90,11 +93,14 @@ void setup()
     Serial.print("..");
     Serial.println();
   }
-  
-  for (byte i = 0; i < _pirTotal; i++) {
-    pinMode(_pirPin[i],INPUT);
-  }
-  
+   
+  //setup PIR pins and attach interrupts
+  pinMode(_pirPin[0], INPUT_PULLUP);
+  pinMode(_pirPin[1], INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(_pirPin[0]), pirInterrupt0, RISING);
+  attachInterrupt(digitalPinToInterrupt(_pirPin[1]), pirInterrupt1, RISING);
+
+  //setup LEDs
   delay(3000);                              //give the power, LED strip, etc. a couple of secs to stabilise
 
   FastLED.setMaxPowerInVoltsAndMilliamps(5, 900);  //limit power draw to 0.9A at 5v
@@ -104,44 +110,39 @@ void setup()
 
   FastLED.clear();
   FastLED.show();
+
+  if (DEBUG) { Serial.println("Setup done."); }
 }
 
-
-void loop()
-{
-  
-  EVERY_N_MILLISECONDS(_pirReadInterval) 
-  {
-    for (byte i = 0; i < _pirTotal; i++) {
-      byte state = digitalRead(_pirPin[i]);
-      if (state == 1 && _pirDebounceTriggered[i] == false) {
-        _pirDebounceTriggered[i] = true;
-        _pirDebouncePrevMillis[i] = millis();
-        _onOff = true;
-        _pirHoldPrevMillis[i] = millis();
-        _pirLastTriggered = i;
-        if (DEBUG) { Serial.println("PIR triggered!"); }
-      } else if (state == 0) { 
-        //if (DEBUG) {  Serial.println("No one!"); } 
-      }
-    }
-  }
-
-  for (byte i = 0; i < _pirTotal; i++) {
-    if (_pirDebounceTriggered[i] == true) {
-      unsigned long pirDebounceCurMillis = millis();    //get current time
-      if((unsigned long) (pirDebounceCurMillis - _pirDebouncePrevMillis[i]) >= _pirDebounceInterval) {
-        //
-        _pirDebounceTriggered[i] = false;
-        if (DEBUG) { Serial.println("PIR debounce interval expired!"); }
-      }
-    }
-  }
+void loop() {
 
   loopPir();
-  
+
   FastLED.show();                           //send all the data to the strips
   FastLED.delay(UPDATES_PER_SECOND);        // (1000/UPDATES_PER_SECOND)
   //
   //delay(_mainLoopDelay);  //using FastLED.delay instead..
 }
+
+void pirInterrupt0() {
+  _pirLastTriggered = 0;  //top
+  pirInterruptPart2();
+  if (DEBUG) { Serial.println("PIR 0 interrupt triggered!"); }
+}
+
+void pirInterrupt1() {
+  _pirLastTriggered = 1;  //bottom
+  pirInterruptPart2();
+  if (DEBUG) { Serial.println("PIR 1 interrupt triggered!"); }
+}
+
+void pirInterruptPart2() {
+  if (_state == 0 || _state == 3) {
+    _state = 1;                               //if off or fading down, fade back up again
+    if (DEBUG) { Serial.println("State set to 1"); }
+    _fadeOnDirection = _pirLastTriggered;
+  }
+  _pirHoldPrevMillis = millis();              //store the current time (reset the timer)
+  _timerRunning = true;                       //enable the timer loop in pir
+}
+
